3c34488288416df7ad264ca6d5592801fcef0650
Aleks164 <alekseylehamail.ru>
2021-10-24 20:55:35 +0300

    DZ_1-3_is_done
    DZ_1 is done but 3 test from 9 is failed. test wint prompt

---
+++ DZ_3/1st ex/calc.js
@@ -0,0 +1,6 @@
+let calc = function() {
+    for (let i = 50; i < 101; i++) {
+    console.log(i);
+  } 
+ };
+ module.exports = calc;
---
+++ DZ_3/2st ex/calc.js
@@ -0,0 +1,7 @@
+let calc = function() {
+    for (let i = 1; i < 10; i++) {
+    console.log(`7x${i}=${i*7}`);
+  } 
+ };
+
+ module.exports = calc;
